e01d77b845815e956ccf9ad8c86f2e853ab3efa9
kacper <kacper.mazur888@gmail.com>
2020-02-17 20:22:50 +0000

    ecs target find

---
+++ TargetFind/Assets/Scripts/TESTfindtarget/FindTargetSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class FindTargetSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.WithNone<HasTarget>().WithAll<Unit>().ForEach((Entity entity, ref Translation unitTranslation )=>
+        {
+            Entity closestTarget = Entity.Null;
+            
+            float3 unitPosition = unitTranslation.Value;
+            float3 closestTargetPosition = float3.zero;
+            
+            
+            Entities.WithAll<Target>().ForEach((Entity targetEntity, ref Translation targetTranslation) =>
+            {
+                if (closestTarget == Entity.Null)
+                {
+                    closestTarget = targetEntity;
+                    closestTargetPosition = targetTranslation.Value;
+                }
+                else
+                {
+                    if (math.distance(unitPosition, targetTranslation.Value) < math.distance(unitPosition, closestTargetPosition))
+                    {
+                        closestTarget = targetEntity;
+                        closestTargetPosition = targetTranslation.Value;
+                    }
+                }
+            });
+
+            if (closestTarget != Entity.Null)
+            {
+
+                PostUpdateCommands.AddComponent(entity, new HasTarget
+                {
+                    TargetEntity = closestTarget
+                });
+            }
+            
+        });
+    }
+}
---
+++ TargetFind/Assets/Scripts/TESTfindtarget/UnitMoveToTargetSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class UnitMoveToTargetSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((Entity unitEntity, ref HasTarget hasTarget, ref Translation translation) =>
+        {
+            if (World.DefaultGameObjectInjectionWorld.EntityManager.Exists(hasTarget.TargetEntity))
+            {
+                Translation targetTranslation =
+                    World.DefaultGameObjectInjectionWorld.EntityManager.GetComponentData<Translation>(
+                        hasTarget.TargetEntity);
+
+                float3 targetDir = math.normalize(targetTranslation.Value - translation.Value);
+                float moveSpeed = 5f;
+
+                translation.Value += targetDir * moveSpeed * Time.DeltaTime;
+
+                if (math.distance(translation.Value, targetTranslation.Value) < 0.2f)
+                {
+                    PostUpdateCommands.DestroyEntity(hasTarget.TargetEntity);
+                    PostUpdateCommands.RemoveComponent(unitEntity, typeof(HasTarget));
+                }
+            }
+            else
+            {
+                PostUpdateCommands.RemoveComponent(unitEntity, typeof(HasTarget));
+            }
+        });
+    }
+}
